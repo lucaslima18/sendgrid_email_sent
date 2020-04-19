727289f43b2da944587d66d385d218f42f038ec9
lucas amorim de lima araújo <lucas.ala1999@gmail.com>
2020-04-19 12:52:06 -0300

    update this structure for one template

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const sgMail = require('@sendgrid/mail');
 const fs = require('fs');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const send_to = "lucas.ala1999@gmail.com"
-const send_from = "startupdoctor@gmail.com"
-const send_subject = "Diagnóstico Startup Doctor"
-const file_name = "diagnostico.jpg"
-const template_id = "d-8290f5ecfe344fc6af191c0a14852055"
-const name = "Lucas Amorim"
-const base64str = base64_encode('default.jpg');
+const send_to = //put the namo of the recipient
+const send_from = //put your emailhere
+const send_subject = //put the email subject here
+const file_name = //put the file name here with your extention ()
+const template_id = //put the template_id of your email template here
+const base64str = base64_encode(file_name);
 
 const msg = {
   to: send_to,
@@ -22,16 +21,20 @@ const msg = {
   ],
   templateId: template_id,
   dynamic_template_data: {
-    nome: name,
+    //if you use dynamic_template_data on your sendgrid template, create constants on your code and put here
   },
 };
 
-function base64_encode(file) {
-    const bitmap = fs.readFileSync(file);
+function base64_encode(file_name) {
+    //this function go encode the file on base64 format
+  
+    const bitmap = fs.readFileSync(file_name);
     return new Buffer.from(bitmap).toString('base64');
 };
 
 function send_email(){
+  //this function go send the email using the sendgrid params
+ 
   (async () => {
     try {
       await sgMail.send(msg);
